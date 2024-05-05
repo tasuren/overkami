49a826366203408642eb2070297c40534f8819bf
Takagi Tasuku <tasuren@outlook.jp>
2024-05-05 18:36:54 +0900

    SolidJSにするので、一度全部削除

--- src/lib/index.ts
+++
@@ -1,6 +0,0 @@
-export function lazyThemeTransitionSetup() {
-	// 最初は一瞬でテーマが反映させるようにする。
-	setTimeout(() => {
-		document.documentElement.style.transition = "background-color 0.2s linear";
-	}, 100);
-}
