6a66ea420b731cb386e344a3b896eceed86ab202
Takagi Tasuku <tasuren@outlook.jp>
2025-06-14 12:51:10 +0900

    Fix display of application path in `WallpaperForm`

--- src/components/wallpaper-view/WallpaperForm.tsx
+++ src/components/wallpaper-view/WallpaperForm.tsx
@@ -97,7 +97,7 @@ export default function WallpaperForm(props: {
                 ],
               })}
             >
-              <Show when={field.value || wallpaper?.application?.name}>
+              <Show when={field.value || wallpaper?.application?.path}>
                 {field.value || wallpaper?.application?.path}
               </Show>
             </div>
