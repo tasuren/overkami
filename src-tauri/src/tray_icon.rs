d08cc138cb3547c09758dfdcc8a56a77213ae7b7
Takagi Tasuku <tasuren@outlook.jp>
2025-07-09 21:16:09 +0900

    Add activation policy management and remove about menu item from tray icon

--- src-tauri/src/tray_icon.rs
+++ src-tauri/src/tray_icon.rs
@@ -4,18 +4,21 @@ pub fn setup_tray_icon(app: &mut tauri::App) {
     let window = app.get_webview_window("main").unwrap();
     window.on_window_event({
         let window = window.clone();
+        let app = app.handle().clone();
 
         move |event| {
             if let tauri::WindowEvent::CloseRequested { api, .. } = event {
                 api.prevent_close();
                 window.hide().expect("Failed to hide main window");
+
+                #[cfg(target_os = "macos")]
+                app.set_activation_policy(tauri::ActivationPolicy::Accessory)
+                    .unwrap();
             }
         }
     });
 
     let menu = MenuBuilder::new(app)
-        .about_with_text("overkamiについて", None)
-        .separator()
         .text("settings", "設定")
         .quit_with_text("overkamiを終了する")
         .build()
@@ -24,20 +27,16 @@ pub fn setup_tray_icon(app: &mut tauri::App) {
     TrayIconBuilder::new()
         .icon(app.default_window_icon().unwrap().clone())
         .menu(&menu)
-        .on_menu_event(move |_app, event| {
-            println!("{event:?}");
+        .on_menu_event(move |app, event| match event.id().as_ref() {
+            "settings" => {
+                window.show().unwrap();
+                window.set_focus().unwrap();
 
-            match event.id().as_ref() {
-                "about" => {
-                    window.show().unwrap();
-                    window.set_focus().unwrap();
-                }
-                "settings" => {
-                    window.show().unwrap();
-                    window.set_focus().unwrap();
-                }
-                _ => {}
+                #[cfg(target_os = "macos")]
+                app.set_activation_policy(tauri::ActivationPolicy::Regular)
+                    .unwrap();
             }
+            _ => {}
         })
         .build(app)
         .expect("Failed to create tray icon");
