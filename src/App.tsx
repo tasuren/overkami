6efc06ecddfef6fa9533dc045fdfe08e7773f6ef
Takagi Tasuku <tasuren@outlook.jp>
2025-11-09 13:21:06 +0900

    feat: add softmedia circle logo

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,19 @@ import { HomeView } from "./components/home-view/HomeView";
 import { default as WallpaperView } from "./components/wallpaper-view/WallpaperView";
 import { GlobalStateProvider, useView } from "./GlobalState";
 import { cl } from "./lib/utils";
+import SofumeLogo from "./sofume_logo.png";
 
 export function Main() {
   const [view] = useView();
 
   return (
     <main style="height: calc(100vh - 48px);">
+      <img
+        src={SofumeLogo}
+        alt="千葉工業大学 ソフトメディア研究会"
+        class="w-auto h-14 absolute -top-1 right-4 opacity-60"
+      />
+
       <HomeView />
 
       <Show when={view().type === "wallpaper"}>
